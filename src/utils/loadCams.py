8e44dc7fefd37215178454444560d7b372e6b000
wilkic <wilkic@gmail.com>
2017-03-03 00:21:22 -0500

    Making cams/spots an actual config (json)
    loading it and populating it with defaults (initializing it)

--- src/utils/loadCams.py
+++ src/utils/loadCams.py
@@ -1,66 +1,24 @@
-import numpy as np
+import json
 
-def loadCameras( time=0, threshSurf=400, edgeLims=[100,200], t2occ=60 ):
+def initCams( config_fname, t2occ=60 ):
     
-    camera1 = {
-        'number': 1,
-        'port': 9001,
-        'im_ts': time,
-        'nFails': 0,
-        'spots': [
-            {
-                'number': 1,
-                'center': [   0,  40],
-                'sTresh': 100,
-                'timePresent': 0,
-                'timeOccupied': 0,
-                'occupationStartTime': time,
-                'occupationEndTime': time,
-                'occupationThresh': t2occ
-            },
-            {
-                'number': 2,
-                'center': [  10,  40],
-                'sTresh': 100,
-                'timePresent': 0,
-                'timeOccupied': 0,
-                'occupationStartTime': time,
-                'occupationEndTime': time,
-                'occupationThresh': t2occ
-            }
-        ]
-    }
-
-    camera2 = {
-        'number': 2,
-        'port': 9002,
-        'im_ts': time,
-        'nFails': 0,
-        'spots': [
-            {
-                'number': 3,
-                'center': [   0,  40],
-                'sTresh': 100,
-                'timePresent': 0,
-                'timeOccupied': 0,
-                'occupationStartTime': time,
-                'occupationEndTime': time,
-                'occupationThresh': t2occ
-            },
-            {
-                'number': 4,
-                'center': [  10,  40],
-                'sTresh': 100,
-                'timePresent': 0,
-                'timeOccupied': 0,
-                'occupationStartTime': time,
-                'occupationEndTime': time,
-                'occupationThresh': t2occ
-            }
-        ]
-    }
-
-    cameras = {1: camera1,
-               2: camera2}
+    # Read spot config file
+    with open(config_fname) as f:
+        cams = json.load(f)
     
-    return cameras
+    default = {
+            'nEdges': 0,
+            'timePresent': 0,
+            'timeOccupied': 0,
+            'occupationStartTime': 0,
+            'occupationEndTime': 0,
+            'occupationThresh': t2occ
+    }
+    
+    for i, c in cams.iteritems():
+        for s in c['spots']:
+            s.update(default)
+
+    return cams
+
+
